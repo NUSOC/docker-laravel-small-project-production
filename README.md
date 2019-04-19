6e75d213c1ce283ae58fd3e6b51168667bcdb370
rag227 <rag227@soc>
2019-04-19 14:00:49 -0500

    init

---
+++ README.md
@@ -0,0 +1,5 @@
+
+Change of plans. Still working on this. 
+
+## Taken from
+https://kyleferg.com/laravel-development-with-docker/?utm_source=laravelfeed.com
---
+++ run.sh
@@ -0,0 +1,6 @@
+
+docker-compose up -d
+docker-compose exec php-apache apt update
+docker-compose exec php-apache cp /additional/000-default.conf /etc/apache2/sites-available/000-default.conf 
+docker-compose exec php-apache service apache2 restart
+docker-compose up -d
