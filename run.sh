9df91cccde065169d2a75f989ff25b5feee71937
rg772 <38052821+rg772@users.noreply.github.com>
2019-04-19 15:35:36 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,7 @@
 docker-compose up -d
 docker-compose exec php-apache apt update 
 docker-compose exec php-apache apt install curl wget vim
-docker-compose exec php-apache curl -sS https://getcomposer.org/installer | sudo php -- --install-dir=/usr/local/bin --filename=composer
+docker-compose exec php-apache curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 docker-compose exec php-apache cp /additional/000-default.conf /etc/apache2/sites-available/000-default.conf 
 docker-compose exec php-apache service apache2 restart
 docker-compose up -d
