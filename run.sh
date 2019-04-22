17254eaaa4c6c7ffe2d21785221b34a2a715bf95
r <root@socdockerweb.soc.northwestern.edu>
2019-04-22 14:48:39 -0500

    something working;

--- run.sh
+++
@@ -1,8 +0,0 @@
-
-docker-compose up -d
-docker-compose exec php-apache apt update 
-docker-compose exec php-apache apt install curl wget vim -y
-docker-compose exec php-apache sh curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
-docker-compose exec php-apache cp /additional/000-default.conf /etc/apache2/sites-available/000-default.conf 
-docker-compose exec php-apache service apache2 restart
-docker-compose start
